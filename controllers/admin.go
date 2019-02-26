f96d04fe717a9fc1bab99e95ad85c6e50047202d
destory <1290417619@qq.com>
2019-02-26 14:49:02 +0800

    整合项目,删除无用文件

--- controllers/admin.go
+++ controllers/admin.go
@@ -25,15 +25,19 @@ func (u *AdminController) Login() {
 	result := &models.LoginResult{}
 	var admin models.Admin
 	json.Unmarshal(u.Ctx.Input.RequestBody, &admin)
-	beego.BeeLogger.Warning("%+v\n", admin)
-	beego.Info(admin.Name)
-	beego.Info(admin.Password)
-	result.Code = 0
-	result.Msg = "OK"
-	result.Data.User = admin
-	result.Data.Token = models.GenToken()
-	result.Data.Name="管理员"
-	result.Data.UUId="admin-uuid"
+	adminName := beego.AppConfig.String("adminName")
+	adminPWD := beego.AppConfig.String("adminPWD")
+	if admin.Name==adminName && admin.Password==adminPWD {
+		result.Code = 0
+		result.Msg = "OK"
+		result.Data.User = admin
+		result.Data.Token = models.GenToken()
+		result.Data.Name="管理员"
+		result.Data.UUId="sinksmell"
+	}else {
+		result.Code=100
+		result.Msg="用户名或密码错误!"
+	}
 	u.Data["json"] = result
 	u.ServeJSON()
 }
--- routers/commentsRouter_controllers.go
+++ routers/commentsRouter_controllers.go
@@ -115,51 +115,6 @@ func init() {
             Filters: nil,
             Params: nil})
 
-    beego.GlobalControllerRouter["MyBlog/controllers:ObjectController"] = append(beego.GlobalControllerRouter["MyBlog/controllers:ObjectController"],
-        beego.ControllerComments{
-            Method: "Post",
-            Router: `/`,
-            AllowHTTPMethods: []string{"post"},
-            MethodParams: param.Make(),
-            Filters: nil,
-            Params: nil})
-
-    beego.GlobalControllerRouter["MyBlog/controllers:ObjectController"] = append(beego.GlobalControllerRouter["MyBlog/controllers:ObjectController"],
-        beego.ControllerComments{
-            Method: "GetAll",
-            Router: `/`,
-            AllowHTTPMethods: []string{"get"},
-            MethodParams: param.Make(),
-            Filters: nil,
-            Params: nil})
-
-    beego.GlobalControllerRouter["MyBlog/controllers:ObjectController"] = append(beego.GlobalControllerRouter["MyBlog/controllers:ObjectController"],
-        beego.ControllerComments{
-            Method: "Get",
-            Router: `/:objectId`,
-            AllowHTTPMethods: []string{"get"},
-            MethodParams: param.Make(),
-            Filters: nil,
-            Params: nil})
-
-    beego.GlobalControllerRouter["MyBlog/controllers:ObjectController"] = append(beego.GlobalControllerRouter["MyBlog/controllers:ObjectController"],
-        beego.ControllerComments{
-            Method: "Put",
-            Router: `/:objectId`,
-            AllowHTTPMethods: []string{"put"},
-            MethodParams: param.Make(),
-            Filters: nil,
-            Params: nil})
-
-    beego.GlobalControllerRouter["MyBlog/controllers:ObjectController"] = append(beego.GlobalControllerRouter["MyBlog/controllers:ObjectController"],
-        beego.ControllerComments{
-            Method: "Delete",
-            Router: `/:objectId`,
-            AllowHTTPMethods: []string{"delete"},
-            MethodParams: param.Make(),
-            Filters: nil,
-            Params: nil})
-
     beego.GlobalControllerRouter["MyBlog/controllers:TopicController"] = append(beego.GlobalControllerRouter["MyBlog/controllers:TopicController"],
         beego.ControllerComments{
             Method: "GetOne",
@@ -228,69 +183,6 @@ func init() {
             Method: "Update",
             Router: `/update`,
             AllowHTTPMethods: []string{"post"},
-            MethodParams: param.Make(),
-            Filters: nil,
-            Params: nil})
-
-    beego.GlobalControllerRouter["MyBlog/controllers:UserController"] = append(beego.GlobalControllerRouter["MyBlog/controllers:UserController"],
-        beego.ControllerComments{
-            Method: "Post",
-            Router: `/`,
-            AllowHTTPMethods: []string{"post"},
-            MethodParams: param.Make(),
-            Filters: nil,
-            Params: nil})
-
-    beego.GlobalControllerRouter["MyBlog/controllers:UserController"] = append(beego.GlobalControllerRouter["MyBlog/controllers:UserController"],
-        beego.ControllerComments{
-            Method: "GetAll",
-            Router: `/`,
-            AllowHTTPMethods: []string{"get"},
-            MethodParams: param.Make(),
-            Filters: nil,
-            Params: nil})
-
-    beego.GlobalControllerRouter["MyBlog/controllers:UserController"] = append(beego.GlobalControllerRouter["MyBlog/controllers:UserController"],
-        beego.ControllerComments{
-            Method: "Get",
-            Router: `/:uid`,
-            AllowHTTPMethods: []string{"get"},
-            MethodParams: param.Make(),
-            Filters: nil,
-            Params: nil})
-
-    beego.GlobalControllerRouter["MyBlog/controllers:UserController"] = append(beego.GlobalControllerRouter["MyBlog/controllers:UserController"],
-        beego.ControllerComments{
-            Method: "Put",
-            Router: `/:uid`,
-            AllowHTTPMethods: []string{"put"},
-            MethodParams: param.Make(),
-            Filters: nil,
-            Params: nil})
-
-    beego.GlobalControllerRouter["MyBlog/controllers:UserController"] = append(beego.GlobalControllerRouter["MyBlog/controllers:UserController"],
-        beego.ControllerComments{
-            Method: "Delete",
-            Router: `/:uid`,
-            AllowHTTPMethods: []string{"delete"},
-            MethodParams: param.Make(),
-            Filters: nil,
-            Params: nil})
-
-    beego.GlobalControllerRouter["MyBlog/controllers:UserController"] = append(beego.GlobalControllerRouter["MyBlog/controllers:UserController"],
-        beego.ControllerComments{
-            Method: "Login",
-            Router: `/login`,
-            AllowHTTPMethods: []string{"get"},
-            MethodParams: param.Make(),
-            Filters: nil,
-            Params: nil})
-
-    beego.GlobalControllerRouter["MyBlog/controllers:UserController"] = append(beego.GlobalControllerRouter["MyBlog/controllers:UserController"],
-        beego.ControllerComments{
-            Method: "Logout",
-            Router: `/logout`,
-            AllowHTTPMethods: []string{"get"},
             MethodParams: param.Make(),
             Filters: nil,
             Params: nil})
